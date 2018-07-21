09a132594795ac2094227730560cffd0548c456c
Thomas Boudreaux <thomas@boudreauxmail.com>
2018-07-21 08:56:34 -0400

    Signifigant Interface Changes
    Allow for dynamic frame loading into animation

--- mplEasyAnimate/animate.py
+++ mplEasyAnimate/animate.py
@@ -1,33 +1,40 @@
 import io
-import matplotlib.pyplot as pyplot
 import imageio
 from scipy.misc import imresize
 from tqdm import tqdm
 
 
 class animation:
-    def __init__(self, filename, figList, size=None):
+    def __init__(self, filename, size=None, pbar=False):
         self.filename = filename
-        self.figList = figList
         self.size = size
-        self.__make_animation__()
+        self.writer = imageio.get_writer(self.filename, mode='I')
+        self.pbar
 
-    def __make_animation__(self):
-        with imageio.get_writer(self.filename, mode='I') as writer:
-            for i, figure in tqdm(enumerate(self.figList), total=len(self.figList)):
-                buf = io.BytesIO()
-                figure.savefig(buf, format='png', bbox_inches='tight')
-                buf.seek(0)
-                image = imageio.imread(buf)
-                if i == 0 and self.size is None:
-                    self.size = image.shape
-                image = imresize(image, self.size)
-                writer.append_data(image)
-                buf.close()
+    def __make_animation_from_raw_list__(self, figList):
+        for i, figure in tqdm(enumerate(figList), total=len(figList), disable=not self.pbar):
+            buf = io.BytesIO()
+            figure.savefig(buf, format='png', bbox_inches='tight')
+            buf.seek(0)
+            image = imageio.imread(buf)
+            if i == 0 and self.size is None:
+                self.size = image.shape
+            image = imresize(image, self.size)
+            self.writer.append_data(image)
+            buf.close()
+
+    def add_frames(self, figList):
+        self.__make_animation_from_raw_list__(figList)
+
+    def close(self):
+        self.writer.close()
+
+    def __del__(self):
+        self.writer.close()
+
 
     def __repr__(self):
         out = list()
-        out.append('Animation made up of: {}'.format(len(self.figList)))
         out.append('Animation size: {}'.format(self.size))
         out.append('Animation path: {}'.format(self.filename))
         return '\n'.join(out)
